7a30c9d4df152ece85f44ad720262bf02632dba5
GarboMuffin <GarboMuffin@users.noreply.github.com>
2020-11-21 21:12:22 +0000

    style: format code

--- addons/block-switching/userscript.js
+++ addons/block-switching/userscript.js
@@ -242,7 +242,7 @@ export default async function ({ addon, global, console, msg }) {
     ];
     blockSwitches["control_forever"] = [
       {
-        opcode: "control_repeat_until"
+        opcode: "control_repeat_until",
       },
       noopSwitch,
     ];
