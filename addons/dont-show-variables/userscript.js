0dd787166e48bd8f195344f61fc30498eb7ae8eb
GarboMuffin <GarboMuffin@users.noreply.github.com>
2021-08-05 21:01:32 +0000

    Format code

--- addons/dont-show-variables/userscript.js
+++ addons/dont-show-variables/userscript.js
@@ -2,15 +2,19 @@ export default async function ({ addon, console }) {
   const ScratchBlocks = await addon.tab.traps.getBlockly();
   const originalCreateVariable = ScratchBlocks.Variables.createVariable;
   ScratchBlocks.Variables.createVariable = function (workspace, callback, type) {
-    return originalCreateVariable(workspace, (id) => {
-      // Undo https://github.com/LLK/scratch-blocks/blob/b67025bd4370d094b836839ba6156804b1e5789c/core/variables.js#L329-L333
-      const flyout = workspace.isFlyout ? workspace : workspace.getFlyout();
-      if (flyout) {
-        flyout.setCheckboxState(id, false);
-      }
-      if (callback) {
-        callback(id);
-      }
-    }, type);
+    return originalCreateVariable(
+      workspace,
+      (id) => {
+        // Undo https://github.com/LLK/scratch-blocks/blob/b67025bd4370d094b836839ba6156804b1e5789c/core/variables.js#L329-L333
+        const flyout = workspace.isFlyout ? workspace : workspace.getFlyout();
+        if (flyout) {
+          flyout.setCheckboxState(id, false);
+        }
+        if (callback) {
+          callback(id);
+        }
+      },
+      type
+    );
   };
 }
