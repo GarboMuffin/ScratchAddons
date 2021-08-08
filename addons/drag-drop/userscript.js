b1f7be5e840fb73124727c7400b0ed87071522d4
Thomas Weber <weber_t@mailbox.org>
2021-08-08 15:25:53 -0500

    drag-drop: don't use better-img-uploads file input

--- addons/drag-drop/userscript.js
+++ addons/drag-drop/userscript.js
@@ -22,7 +22,7 @@ export default async function ({ addon, global, console }) {
       (el = e.target.closest('div[class*="selector_wrapper"]'))
     ) {
       callback = (files) => {
-        const fileInput = el.querySelector('input[class*="action-menu_file-input"]');
+        const fileInput = el.querySelector('input[class*="action-menu_file-input"]:not(.sa-better-img-uploads-file-input)');
         fileInput.files = files;
         fileInput.dispatchEvent(new Event("change", { bubbles: true }));
       };
