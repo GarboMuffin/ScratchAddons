39ab4916de183f4260be4e74f8f285f1663ce3c7
Thomas Weber <muffin@muffin.ink>
2022-02-22 22:01:15 -0600

    swap-local-global: slightly optimize (?) selector

--- addons/swap-local-global/userscript.js
+++ addons/swap-local-global/userscript.js
@@ -221,7 +221,7 @@ export default async function ({ addon, msg, console }) {
   };
 
   const canUserUseCloudVariables = () => {
-    const blocksWrapper = document.querySelector('[class^="gui_blocks-wrapper_"]');
+    const blocksWrapper = document.querySelector('div[class^="gui_blocks-wrapper_"]');
     let internalNode = blocksWrapper[addon.tab.traps.getInternalKey(blocksWrapper)];
     while (true) {
       if (!internalNode) {
